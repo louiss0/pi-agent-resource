cf1a8b231edf29381788ea8754c946b55c4b7f2c
louiss0 <louisshelton0@gmail.com>
2026-04-15 16:53:13 -0400

    feat: create general sub command utilities
    I decided to create utilities for parsing arguments!
    I decided that create, edit and delete were the commands.
    I wanted to create for all the extentions I wanted to make!

---
+++ shared/subcommands.ts
@@ -0,0 +1,28 @@
+import { picklist, safeParse } from "valibot";
+
+const SUBCOMMANDS = picklist(["create", "edit", "delete"]);
+
+export function parseSubCommandValuesFromArgument(argument: string) {
+  return safeParse(SUBCOMMANDS, argument);
+}
+
+function generateSubcommandArgumentCompletionsUsingSubLabel(subLabel: string) {
+  return SUBCOMMANDS.options.map((option) =>
+    option === "create"
+      ? {
+          label: `${option}:${subLabel}`,
+          value: option,
+          description: `${option[0].toUpperCase()}${option.substring(1)} a new ${subLabel}`,
+        }
+      : {
+          label: `${option}:${subLabel}`,
+          value: option,
+          description: `${option[0].toUpperCase()}${option.substring(1)} a ${subLabel}`,
+        },
+  );
+}
+
+export function getFilterSubcommandArgumentCompletionFromStringUsingSubLabel(subLabel: string) {
+  const completions = generateSubcommandArgumentCompletionsUsingSubLabel(subLabel);
+  return (value: string) => completions.filter((completion) => completion.value === value);
+}
