c1062d665ee5b54c0d9d8c0e0274adc4ddf95bad
louiss0 <louisshelton0@gmail.com>
2026-04-15 22:41:29 -0400

    refactor(skill-creator): create testable helpers
    I decided to create helpers to make command handlers resuable!

--- extensions/skill-creator.ts
+++ extensions/skill-creator.ts
@@ -1,33 +1,58 @@
-import { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+import type {
+  ExtensionAPI,
+  ExtensionContext,
+  RegisteredCommand,
+} from "@mariozechner/pi-coding-agent";
 import {
   getFilterSubcommandArgumentCompletionFromStringUsingSubLabel,
   parseSubCommandValuesFromArgument,
 } from "../shared/subcommands";
+import { summarize } from "valibot";
+
 export default (pi: ExtensionAPI) => {
   pi.registerCommand("resource:skill", {
     description: "This is for creating a new skill",
     getArgumentCompletions:
       getFilterSubcommandArgumentCompletionFromStringUsingSubLabel("skill"),
-
-    handler: async (arg, ctx) => {
-      const result = parseSubCommandValuesFromArgument(arg);
-
-      if (!result.success) {
-        ctx.ui.notify(`Invalid command: ${result.issues[0].message}`);
-        return;
-      }
-
-      switch (result.output) {
-        case "create":
-          ctx.ui.notify("Skill created successfully");
-          break;
-        case "edit":
-          ctx.ui.notify("Skill edited successfully");
-          break;
-        case "delete":
-          ctx.ui.notify("Skill deleted successfully");
-          break;
-      }
-    },
+    handler: generateCommandHandlerUsingDeps({}),
   });
 };
+
+type Dependencies = Record<string, unknown>;
+
+export function generateCommandHandlerUsingDeps(
+  dependency: Dependencies,
+): RegisteredCommand["handler"] {
+  return async (arg, ctx) => {
+    const result = parseSubCommandValuesFromArgument(arg);
+
+    if (!result.success) {
+      ctx.ui.notify(`Invalid command: ${summarize(result.issues)}`, "error");
+      return;
+    }
+
+    switch (result.output) {
+      case "create":
+        handleCreate(ctx);
+        break;
+      case "edit":
+        handleEdit(ctx);
+        break;
+      case "delete":
+        handleDelete(ctx);
+        break;
+    }
+  };
+}
+
+function handleCreate(ctx: ExtensionContext) {
+  ctx.ui.notify("Skill created successfully");
+}
+
+function handleEdit(ctx: ExtensionContext) {
+  ctx.ui.notify("Skill edited successfully");
+}
+
+function handleDelete(ctx: ExtensionContext) {
+  ctx.ui.notify("Skill deleted successfully");
+}
